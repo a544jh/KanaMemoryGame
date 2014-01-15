ccd9db6af89751cf40b9d94c2bdb5acfd2977526
a544jh <axel.wikstrom@gmail.com>
2014-01-15 22:09:45 +0200

    added the NetBeans project

---
+++ KanaMemory/src/main/java/com/a544jh/kanamemory/App.java
@@ -0,0 +1,13 @@
+package com.a544jh.kanamemory;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
