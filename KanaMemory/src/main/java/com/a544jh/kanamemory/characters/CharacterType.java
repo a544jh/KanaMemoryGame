5c856bb0ed38293f3b52944c1b5e05f82778e983
a544jh <axel.wikstrom@gmail.com>
2014-01-20 20:07:20 +0200

    adding class files

---
+++ KanaMemory/src/main/java/com/a544jh/kanamemory/characters/CharacterType.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.a544jh.kanamemory.characters;
+
+/**
+ *
+ * @author Axel
+ */
+public enum CharacterType {
+    HIRAGANA, KATAKANA, ROMAJI
+}
---
+++ KanaMemory/src/main/java/com/a544jh/kanamemory/characters/KanaSyllables.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.a544jh.kanamemory.characters;
+
+/**
+ *
+ * @author Axel
+ */
+public enum KanaSyllables {
+
+    A("あ", "ア"),
+    I("\u3044", "\u30a4");
+
+    private final String hiragana, katakana;
+
+    private KanaSyllables(String hiragana, String katakana) {
+        this.hiragana = hiragana;
+        this.katakana = katakana;
+    }
+
+    public String getHiragana() {
+        return hiragana;
+    }
+
+    public String getKatakana() {
+        return katakana;
+    }
+
+    public String getRomaji() {
+        return this.toString().toLowerCase();
+    }
+}
