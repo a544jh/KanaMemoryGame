2c41dcf609115789764b7645eea45622e586c9f3
a544jh <axel.wikstrom@gmail.com>
2014-01-20 20:05:22 +0200

    Made some classes. Trying to achieve proper console output.

--- KanaMemory/src/main/java/com/a544jh/kanamemory/App.java
+++ KanaMemory/src/main/java/com/a544jh/kanamemory/App.java
@@ -1,13 +1,22 @@
 package com.a544jh.kanamemory;
 
+import com.a544jh.kanamemory.characters.*;
+import java.io.PrintStream;
+import java.io.UnsupportedEncodingException;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+
+    public static void main(String[] args) throws UnsupportedEncodingException {
+        System.out.println("Hello World!");
+        System.out.println(KanaSyllables.A.getHiragana());
+        PrintStream out = new PrintStream(System.out, true, "UTF-8");
+        out.println(KanaSyllables.A.getHiragana());
+        System.out.println("あ");
+        System.out.println(KanaSyllables.A.getRomaji());
+        System.out.println(KanaSyllables.I.getKatakana());
     }
 }
